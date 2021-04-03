43c8649363cde6e37dc1fe3b4e2ec3db007e1fbf
Mikhail Kortunov <mikhailkortunov@yandex.ru>
2021-04-03 18:13:25 +0300

    Solved

--- Math_thinking.py
+++ Math_thinking.py
@@ -5,10 +5,15 @@ def cards(lower_limit, upper_limit):
     pairs =[]
     for i in x:
         for j in y:
-            if (i+j == 100 and i!=j):
+            if (i+j != 100 and i!=j):
                 pairs += [[i,j]]
             else:
                 continue
     return pairs
 
 print(len(cards(10,100)))
+
+
+for x in range(3, 100):
+    if (x % 3 == 2 and x % 4 == 3 and x % 5 == 4):
+        print(x)
